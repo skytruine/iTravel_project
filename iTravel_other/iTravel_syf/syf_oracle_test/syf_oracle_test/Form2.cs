c66e8bfc03ffe7148567acbc1f358414a128b1de
skytruine <1159331173@qq.com>
2017-05-06 22:46:33 +0800

    创建核心数据集+完成服务器端测试
    创建核心数据集+完成服务器端测试
    iTravel_Map_v0.1中是我们的当前的地图，里面的网络数据集什么的一应俱全

---
+++ iTravel_other/iTravel_syf/syf_oracle_test/syf_oracle_test/Form2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OracleClient;
+
+namespace syf_oracle_test
+{
+    public partial class Form2 : Form
+    {
+        string connString;
+        OracleConnection conn;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sqlString;
+            string accountString;
+            string passwordConfirmString;
+            string passwordString;
+            int signFlag;
+
+            connString = "data source=120.24.97.173/orcl;User Id=system;Password=517012;";
+            conn = new OracleConnection(connString);
+            try
+            {
+                conn.Open();
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("数据库连接异常");
+
+            }
+            OracleCommand cmd;
+            cmd = conn.CreateCommand();
+
+            this.Close();
+        }
+    }
+}
---
+++ iTravel_other/iTravel_syf/syf_oracle_test/syf_oracle_test/dbOperater.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.OracleClient;
+namespace syf_oracle_test
+{
+    class dbOperater
+    {
+        //
+        
+    
+    }
+}
